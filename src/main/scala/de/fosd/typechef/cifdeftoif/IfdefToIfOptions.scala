0cd6805d66741a5cea32f19c6d8e4baaf55a2866
Jörg Liebig <joliebig@fim.uni-passau.de>
2014-06-11 12:19:01 +0200

    move decluse option from frontendoptions to ifdeftoifoptions

--- src/main/scala/de/fosd/typechef/cifdeftoif/IfdefToIfOptions.scala
+++ src/main/scala/de/fosd/typechef/cifdeftoif/IfdefToIfOptions.scala
@@ -10,10 +10,12 @@ class IfdefToIfOptions extends FrontendOptionsWithConfigFiles {
     private final val F_IFDEFTOIFSTATISTICS: Char = Options.genOptionId
     private final val F_IFDEFTOIFNOCHECK: Char = Options.genOptionId
     private final val F_FEATURECONFIG: Char = Options.genOptionId
+    private final val F_DECLUSE: Char = Options.genOptionId
 
     var ifdeftoif: Boolean = false
     var ifdeftoifstatistics: Boolean = false
     var ifdeftoifnocheck: Boolean = false
+    var decluse: Boolean = false
     private var featureConfigFile: String = ""
     private var includeStructFile: String = ""
 
@@ -29,7 +31,9 @@ class IfdefToIfOptions extends FrontendOptionsWithConfigFiles {
                 new Options.Option("ifdeftoifnocheck", LongOpt.NO_ARGUMENT, F_IFDEFTOIFNOCHECK, "file",
                     "Do not typecheck the result of #ifdef to if transformation."),
                 new Options.Option("featureConfig", LongOpt.REQUIRED_ARGUMENT, F_FEATURECONFIG, null,
-                    "Make #ifdef to if transformation.")
+                    "Make #ifdef to if transformation."),
+                new Options.Option("decluse", LongOpt.NO_ARGUMENT, F_DECLUSE, null,
+                    "Test the declaration use map.")
             ))
 
         groups
@@ -64,6 +68,10 @@ class IfdefToIfOptions extends FrontendOptionsWithConfigFiles {
             typecheck = true
             ifdeftoif = true
             ifdeftoifnocheck = true
+        } else if (c == F_DECLUSE) {
+            parse = true
+            typecheck = true
+            decluse = true
         } else {
             return super.interpretOption(c, g)
         }
