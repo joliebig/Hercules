04dad6d960dfc43c1e4e92cf059cda5e0fa2138a
Florian Garbe <fgarbe@fim.uni-passau.de>
2014-06-19 23:26:43 +0200

    lowered JVM parameters in mkrun.sh

--- mkrun.sh
+++ mkrun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx1024M -Xss10M -XX:PermSize=256M -jar sbt-launch.jar compile copy-resources mkrun
+java -Xmx768M -Xss10M -XX:PermSize=256M -jar sbt-launch.jar compile copy-resources mkrun
