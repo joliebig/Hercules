45e365482054d3675c26f0bc49ec71ffc3a92f45
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-03-02 21:42:27 +0100

    removed '-XX:MaxPermSize=' and '-XX:PermSize=' options, java 8 VM does not support them

--- mkrun.sh
+++ mkrun.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $0)
 
-java -Xmx768M -Xss10M -XX:PermSize=256M -jar sbt-launch.jar compile copy-resources mkrun
+java -Xmx768M -Xss10M -jar sbt-launch.jar compile copy-resources mkrun
